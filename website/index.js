33b3768aa97b514a025fad36bbc45d8648e5ec90
Heqo <153826194+4Oliver@users.noreply.github.com>
2024-05-01 17:25:45 +0200

    Update index.js

--- website/index.js
+++ website/index.js
@@ -1,7 +1,47 @@
-import { io } from "./socket.io.esm.min.js";
+import { io } from './socket.io.esm.min.js';
+const userChat = document.getElementById('user-chat');
+const chatContainer = document.getElementById('chat-container');
+const imageContainer = document.getElementById('image-container');
+const chatInput = document.getElementById('chat-input');
 
 const socket = io();
 
-socket.on("welcome", (data) => {
-  console.log('got welcome!', [data]);
+let lastMessageSentBy;
+
+socket.on('welcome', (data) => {
+	console.log('got welcome!', [data]);
 });
+
+socket.on('message', (data) => {
+	lastMessageSentBy = data.last;
+	newMessage(data.message, data.pfp, socket);
+});
+
+userChat.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const message = chatInput.value;
+	socket.emit('message', message);
+	chatInput.value = '';
+});
+
+function newMessage(message, pfp, socket) {
+	console.log(pfp);
+	const messageElement = document.createElement('div');
+	console.log(socket.id);
+	console.log(lastMessageSentBy);
+
+	const imageElement = document.createElement('img');
+	imageElement.src = pfp;
+	imageElement.alt = 'user_pfp';
+	imageElement.style.width = '100px';
+	imageElement.style.height = '100px';
+	imageContainer.appendChild(imageElement);
+
+	/* const imageElement = document.createElement('img');
+	imageElement.src = pfp;
+	imageElement.alt = 'user_pfp';
+	imageElement.style.width = '100px';
+	imageElement.style.height = '100px'; */
+	messageElement.innerText = message;
+	imageContainer.append(messageElement);
+}
